3d81a4d01ba302002dc2076811c489852b1f0f8e
Emily Kersey <ekerseyc@gmail.com>
2022-04-06 13:24:20 -0400

    changed instances of project to event

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Event } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newEvent = await Event.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newEvent);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -16,19 +16,19 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const eventData = await Event.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!eventData) {
+      res.status(404).json({ message: 'No event found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(eventData);
   } catch (err) {
     res.status(500).json(err);
   }
