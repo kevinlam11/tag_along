06b8d19476b7d4cdc30411fd4e76aec9c1b171e9
Emily Kersey <ekerseyc@gmail.com>
2022-04-08 14:04:07 -0400

    prep for pull

--- public/js/events.js
+++ public/js/events.js
@@ -1,6 +1,6 @@
 // to do: if there is a new event added, data is compiled into a container
 // event name
-// to do: moment.js implementation into date and time
+// to do: make date time local string into something humans can read
 // user name
 // description
 // how many people have RSVP'd
