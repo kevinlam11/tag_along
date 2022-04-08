05b0cea8a1147110d98a407c1a28cb5ba9e8d716
Carrington <hartis.c99@gmail.com>
2022-04-08 14:05:24 -0400

    newevent.js

--- public/js/newevent.js
+++ public/js/newevent.js
@@ -1,29 +1,29 @@
 // to do: if there is a new event added, data is compiled into a container
-function createEvent(event) {
-  event.preventDefault();
+// function createEvent(event) {
+//   event.preventDefault();
 
-  const title = document.getElementById('eventName').value.trim();
-  const description = document.getElementById('eventDesc').value.trim();
-  const dt = document.querySelector('#dateTime').value;
+//   const title = document.getElementById('eventName').value.trim();
+//   const description = document.getElementById('eventDesc').value.trim();
+//   const dt = document.querySelector('#dateTime').value;
 
-  if (event.target.matches("#createEvent")) {
-    console.log(dt)
+//   if (event.target.matches("#createEvent")) {
+//     console.log(dt)
   
-    if (title && description && dt) {
-      fetch('/api/events', {
-        method: 'POST',
-        body: JSON.stringify({ title, description }),
-        headers: { 'Content-Type': 'application/json' },
-      }).then((response) => {
-        return response.json()
-      }).then((data) => {
-        console.log(data);
-      })
-    }
-  }
-}
+//     if (title && description && dt) {
+//       fetch('/api/events', {
+//         method: 'POST',
+//         body: JSON.stringify({ title, description }),
+//         headers: { 'Content-Type': 'application/json' },
+//       }).then((response) => {
+//         return response.json()
+//       }).then((data) => {
+//         console.log(data);
+//       })
+//     }
+//   }
+// }
 
-document.addEventListener('click', createEvent);
+// document.addEventListener('click', createEvent);
 // when new event is clicked, a modal pops up with the form
 // when form submit is hit, input data is stored
 // when form submit is hit, modal displays Event Created! and a close modal button
