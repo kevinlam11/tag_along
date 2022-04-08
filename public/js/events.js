e3c093cbebaa422d2c08f05bd8a3abdf0e2b95cb
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-04-08 15:40:44 -0400

    updated js

--- public/js/events.js
+++
@@ -1,8 +0,0 @@
-// to do: if there is a new event added, data is compiled into a container
-// event name
-// to do: moment.js implementation into date and time
-// user name
-// description
-// how many people have RSVP'd
-
-// to do: rsvp badge functionality
